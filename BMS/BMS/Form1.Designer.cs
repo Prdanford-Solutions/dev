3dc1003a0963254cbe4a67512a12cd4d92d77c04
Paul Danford <paul@prdanford.co.uk>
2024-12-11 12:35:33 +0000

    Latest dev

--- BMS/BMS/Form1.Designer.cs
+++ BMS/BMS/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtTextToSpeak = new System.Windows.Forms.TextBox();
+            this.btnSayIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTextToSpeak
+            // 
+            this.txtTextToSpeak.Location = new System.Drawing.Point(241, 103);
+            this.txtTextToSpeak.Name = "txtTextToSpeak";
+            this.txtTextToSpeak.Size = new System.Drawing.Size(293, 20);
+            this.txtTextToSpeak.TabIndex = 1;
+            // 
+            // btnSayIt
+            // 
+            this.btnSayIt.Location = new System.Drawing.Point(241, 166);
+            this.btnSayIt.Name = "btnSayIt";
+            this.btnSayIt.Size = new System.Drawing.Size(75, 23);
+            this.btnSayIt.TabIndex = 2;
+            this.btnSayIt.Text = "Say It!";
+            this.btnSayIt.UseVisualStyleBackColor = true;
+            this.btnSayIt.Click += new System.EventHandler(this.btnSayIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSayIt);
+            this.Controls.Add(this.txtTextToSpeak);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtTextToSpeak;
+        private System.Windows.Forms.Button btnSayIt;
     }
 }
 
--- BMS/BMS/Form1.cs
+++ BMS/BMS/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace BMS
 {
@@ -15,6 +17,17 @@ namespace BMS
         public Form1()
         {
             InitializeComponent();
+
+            txtTextToSpeak.Text = "Hello my name is Paul and you smell really really bad";
+
+            btnSayIt.Focus();
+        }
+
+        private void btnSayIt_Click(object sender, EventArgs e)
+        {
+            var synthesizer = new SpeechSynthesizer();
+
+            synthesizer.Speak(txtTextToSpeak.Text);
         }
     }
 }
